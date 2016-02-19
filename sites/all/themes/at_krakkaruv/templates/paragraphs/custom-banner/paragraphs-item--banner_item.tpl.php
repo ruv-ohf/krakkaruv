8fca8ee5364b30b85d67d6f8b95a3b10a62fee2b
unknown <franze@Lenovo-PC.ruv.is>
2016-02-19 15:28:45 +0000

    theming paragraphs for custom banners

---
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
@@ -0,0 +1,32 @@
+<?php
+$image_url = '';
+
+//Til að fá atom id
+$atom_id = $content['field_custom_banner_image']['#items'][0]['sid'];
+
+//til að ná í atom
+$atom = scald_atom_load($atom_id);
+
+if(!empty($atom)) {
+        //Til að ná í myndaurlið í réttum stíl
+	$image_url = image_style_url('hero_image_subpage', $atom->file_source);
+}
+
+?>
+
+<?php
+	hide($content['field_custom_banner_height']);
+	hide($content['field_custom_banner_image']);
+	hide($content['field_background_repeat']);
+	hide($content['field_breakpoint']);
+	print render($content);
+?>
+
+<style>
+	
+	.paragraphs-items {
+		height: <?php ($content['field_custom_banner_height']);?>;
+		background: url(<?php print $image_url; ?>) no-repeat center center;
+	}
+
+</style>
