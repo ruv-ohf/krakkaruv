8fc390a4ab7019dcf9e68eeac7c545a187ebf6e3
unknown <franze@Lenovo-PC.ruv.is>
2016-02-19 14:52:08 +0000

    theming paragraphs for custom banners

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
@@ -7,6 +7,11 @@ $atom_id = $content['field_custom_banner_image']['#items'][0]['sid'];
 //til að ná í atom
 $atom = scald_atom_load($atom_id);
 
+if(!empty($atom)) {
+        //Til að ná í myndaurlið í réttum stíl
+	$image_url = image_style_url('', $atom->file_source);
+}
+
 ?>
 
 <?php
