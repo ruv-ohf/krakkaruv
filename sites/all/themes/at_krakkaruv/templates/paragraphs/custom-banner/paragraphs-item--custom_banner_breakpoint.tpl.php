738ad219cec819ba947f31f5046968c3b91beabc
unknown <franze@Lenovo-PC.ruv.is>
2016-02-19 12:02:42 +0000

    theming paragraphs for custom banners

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
@@ -1,6 +1,16 @@
 <?php
-	$image_url = $content['field_custom_banner_image'];
-    $speaker_image_url = "'$image_url'";
+// field_my_image is the name of the image field
+  
+  // using field_get_items() you can get the field values (respecting multilingual setup)
+  $field_my_image = field_get_items('node', $node, 'field_custom_banner_image');
+  
+  // after you have the values, you can get the image URL (you can use foreach here)
+  $my_image_url = file_create_url($field_my_image[0]['uri']);
+  
+  print $my_image_url;
+?>
+
+<?php
 	hide($content['field_min_width_size']);
 	hide($content['field_custom_banner_image']);
 	print render($content);
