5741dc4966f509341eb738aa2dc7abf7592f1d97
unknown <franze@Lenovo-PC.ruv.is>
2016-02-19 10:27:01 +0000

    theming paragraphs for custom banners

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
@@ -1,12 +1,13 @@
 <?php
 	hide($content['field_min_width_size']);
+	hide($content['field_custom_banner_image']);
 	print render($content);
 ?>
 
 <style>
 	@media only screen and (min-width: <?php print render($content['field_min_width_size']); ?>px) {
 		.paragraphs-items {
-			background-color: red;
+			background: #071815 url(/sites/default/files/thumbnails/image/<?php print render($content['field_custom_banner_image']); ?>) no-repeat center center;
 		}
 	}
 </style>
