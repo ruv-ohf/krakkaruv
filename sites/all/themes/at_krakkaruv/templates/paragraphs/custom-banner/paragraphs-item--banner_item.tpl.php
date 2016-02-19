af845468ba7bb8c3e45797fe4fec58258b7f8268
unknown <franze@Lenovo-PC.ruv.is>
2016-02-19 09:43:52 +0000

    theming paragraphs for custom banners

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
@@ -1,3 +1,9 @@
 <div class="paragraphs-item">
-  <?php print render($content['field_min_width_size']); ?>
+<?php
+	$node = node_load($nid);
+	$field = field_get_items('node', $node, 'field_min_width_size');
+	$output = field_view_value('node', $node, 'field_min_width_size', $field[$delta]);
+?>
+	<?php print $output; ?>
+  	<?php //print render($content['field_min_width_size']); ?>
 </div>
