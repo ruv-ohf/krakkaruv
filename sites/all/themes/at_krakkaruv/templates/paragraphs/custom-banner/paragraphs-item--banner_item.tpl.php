e11563b82222501fae94bc7ebe0566001fc46518
unknown <franze@Lenovo-PC.ruv.is>
2016-02-19 09:53:14 +0000

    theming paragraphs for custom banners

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
@@ -1,7 +1,9 @@
 <div class="paragraphs-item">
-<?php
-	$output = field_view_value('node', $node, 'field_min_width_size', $field[$delta]);
-?>
-	<?php print render($output); ?>
-  	<?php //print render($content['field_min_width_size']); ?>
+	
+	<?php
+	    hide($content['field_min_width_size']);
+	    print render($content);
+    ?>
+
+	<?php print render($content['field_min_width_size']); ?>
 </div>
