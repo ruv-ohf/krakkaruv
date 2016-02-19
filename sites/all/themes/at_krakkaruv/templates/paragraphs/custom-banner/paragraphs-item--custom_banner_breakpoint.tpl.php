e9e445d5a31fcc6d8b8574bc3b34483b01055b54
unknown <franze@Lenovo-PC.ruv.is>
2016-02-19 12:10:56 +0000

    theming paragraphs for custom banners

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
@@ -1,13 +1,8 @@
 <?php
-// field_my_image is the name of the image field
+
+  //$my_image_url = file_create_url($field_my_image[0]['uri']);
   
-  // using field_get_items() you can get the field values (respecting multilingual setup)
-  $field_my_image = field_get_items('node', $content, 'field_custom_banner_image');
-  
-  // after you have the values, you can get the image URL (you can use foreach here)
-  $my_image_url = file_create_url($field_my_image[0]['uri']);
-  
-  print $my_image_url;
+
 ?>
 
 <?php
@@ -19,7 +14,7 @@
 <style>
 	@media only screen and (min-width: <?php print render($content['field_min_width_size']); ?>px) {
 		.paragraphs-items {
-			background: #071815 url(<?php print $speaker_image_url; ?>) no-repeat center center;
+			background: #071815 url(<?php print render(file_create_url($content['field_custom_banner_image'])); ?>) no-repeat center center;
 			height: 250px;
 		}
 	}
