260aaa56bd259f40f65328f7d2d111b8927e6959
unknown <franze@Lenovo-PC.ruv.is>
2016-02-19 13:09:10 +0000

    theming paragraphs for custom banners

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
@@ -1,7 +1,7 @@
 <?php
 
   $my_image_url = file_create_url($field_custom_banner_image[0]['uri']);
-  $my_image_style = image_style_url($style, $field_custom_banner_image['und'][0]['uri']);
+  $my_image_style = image_style_url($style, render($content($field_custom_banner_image['und'][0]['uri']));
   //print render($my_image_url);
   print render($my_image_style);
 ?>
