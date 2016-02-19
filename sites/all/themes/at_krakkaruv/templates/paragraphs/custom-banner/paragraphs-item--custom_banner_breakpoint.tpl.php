f1516c78165d901d4710b7cd7eca482ad3aa4ea5
unknown <franze@Lenovo-PC.ruv.is>
2016-02-19 14:45:58 +0000

    theming paragraphs for custom banners

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
@@ -22,7 +22,6 @@ if(!empty($atom)) {
 <style>
 	@media only screen and (min-width: <?php print render($content['field_min_width_size']); ?>px) {
 		.paragraphs-items {
-			background: #071815 url() no-repeat center center;
 			height: 250px;
 		}
 	}
