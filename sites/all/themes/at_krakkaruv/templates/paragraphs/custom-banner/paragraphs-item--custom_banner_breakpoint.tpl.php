b7a5f321f54ce968126e18746b4fb6f2ee5d42d4
unknown <franze@Lenovo-PC.ruv.is>
2016-02-19 14:28:02 +0000

    theming paragraphs for custom banners

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
@@ -1,16 +1,27 @@
 <?php
+$image_url = '';
+
+//Til að fá atom id
+$atom_id = $content['field_custom_banner_image']['#items'][0]['sid'];
+
+//til að ná í atom
+$atom = scald_atom_load($atom_id);
+
+if(!empty($atom)) {
+        //Til að ná í myndaurlið í réttum stíl
+	$image_url = image_style_url('thumbnails', $atom->file_source);
+?>
+
+<?php
 	hide($content['field_min_width_size']);
 	hide($content['field_custom_banner_image']);
 	print render($content);
 ?>
 
-<pre>
-<?php print_r($fields['field_custom_banner_image']->content); ?>
-
 <style>
 	@media only screen and (min-width: <?php print render($content['field_min_width_size']); ?>px) {
 		.paragraphs-items {
-			background: #071815 url(/sites/default/files/thumbnails/image/<?php print render($content['field_custom_banner_image']); ?>) no-repeat center center;
+			background: #071815 url(<?php $image_url; ?>); ?>) no-repeat center center;
 			height: 250px;
 		}
 	}
