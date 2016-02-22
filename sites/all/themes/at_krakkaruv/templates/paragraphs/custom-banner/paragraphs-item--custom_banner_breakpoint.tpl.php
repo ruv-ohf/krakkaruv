2f762717a853a31cee2b60a59ca094245a65f461
unknown <franze@Lenovo-PC.ruv.is>
2016-02-22 09:37:08 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--custom_banner_breakpoint.tpl.php
@@ -14,12 +14,6 @@ if(!empty($atom)) {
 
 ?>
 
-<?php
-	hide($content['field_min_width_size']);
-	hide($content['field_custom_banner_image']);
-	print render($content);
-?>
-
 <style>
 	@media only screen and (min-width: <?php print render($content['field_min_width_size']); ?>px) {
 		.paragraphs-items {
