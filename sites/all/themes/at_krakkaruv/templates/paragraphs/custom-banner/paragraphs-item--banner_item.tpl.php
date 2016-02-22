2de512fd32936ab8eeafc25a187dc869b71cb4a3
unknown <franze@Lenovo-PC.ruv.is>
2016-02-22 12:12:44 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
@@ -41,7 +41,7 @@ if(!empty($atom2)) {
 		background: url(<?php print $image_url2; ?>) repeat-x left top;
 	}
 
-	.paragraphs-items-wrapper-inner a {
+	.paragraphs-items-wrapper a {
 		display: block;
 	}
 
