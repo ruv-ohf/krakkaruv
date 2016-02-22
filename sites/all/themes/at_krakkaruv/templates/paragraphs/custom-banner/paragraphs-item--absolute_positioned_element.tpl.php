c2b5afaac1747bb8fcc38f7bd87a45f209135402
unknown <franze@Lenovo-PC.ruv.is>
2016-02-22 13:45:08 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--absolute_positioned_element.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--absolute_positioned_element.tpl.php
@@ -1,1 +1,28 @@
-<h1>Custom Banner</h1>
+<?php hide($content['field_custom_banner_image']); ?>
+<?php hide($content['field_custom_banner_top_position']); ?>
+
+<?php
+$image_url = '';
+
+//Til að fá atom id
+$atom_id = $content['field_custom_banner_image']['#items'][0]['sid'];
+
+//til að ná í atom
+$atom = scald_atom_load($atom_id);
+
+if(!empty($atom)) {
+        //Til að ná í myndaurlið í réttum stíl
+	$image_url = image_style_url('hero_image_subpage', $atom->file_source);
+}
+
+?>
+
+<h1>Position</h1>
+
+<style>
+	@media only screen and (min-width: <?php print render($content['field_min_width_size']); ?>px) {
+		.paragraphs-items-field-absolute-positioned-elemen {
+			top: <?php print render($content['field_custom_banner_top_position']); ?>;
+		}
+	}
+</style>
