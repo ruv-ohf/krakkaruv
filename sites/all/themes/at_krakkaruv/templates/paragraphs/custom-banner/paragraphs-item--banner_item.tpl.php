60e26c80549228c6963549cae49510f7835066aa
unknown <franze@Lenovo-PC.ruv.is>
2016-02-22 10:09:42 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
@@ -3,13 +3,19 @@ $image_url = '';
 
 //Til að fá atom id
 $atom_id = $content['field_custom_banner_image']['#items'][0]['sid'];
+$atom_id2 = $content['field_background_repeat']['#items'][0]['sid'];
 
 //til að ná í atom
 $atom = scald_atom_load($atom_id);
+$atom2 = scald_atom_load($atom_id2);
 
 if(!empty($atom)) {
         //Til að ná í myndaurlið í réttum stíl
 	$image_url = image_style_url('hero_image_subpage', $atom->file_source);
+}
+if(!empty($atom2)) {
+        //Til að ná í myndaurlið í réttum stíl
+	$image_url2 = image_style_url('hero_image_subpage', $atom->file_source);
 }
 
 ?>
@@ -28,7 +34,7 @@ if(!empty($atom)) {
 		background: url(<?php print $image_url; ?>) no-repeat center center;
 	}
 	.bg-repeat {
-		background: url(<?php print render($content['field_background_repeat']); ?>) repeat-x left top;
+		background: url(<?php print image_url2; ?>) repeat-x left top;
 	}
 
 </style>
