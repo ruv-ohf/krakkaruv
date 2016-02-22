74839b30086313efc10e0ac1436d1a9d0f1c0269
unknown <franze@Lenovo-PC.ruv.is>
2016-02-22 14:57:51 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--absolute_positioned_element.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--absolute_positioned_element.tpl.php
@@ -20,4 +20,4 @@ if(!empty($atom)) {
 <div class="absolute-image"><img src="<?php print$image_url; ?>"></div>
 
 <pre>
-<?php print_r($content['field_custom_banner_image']); ?>
+<?php print_r($image_url); ?>
