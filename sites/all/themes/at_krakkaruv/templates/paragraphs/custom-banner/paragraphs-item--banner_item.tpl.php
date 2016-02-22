3d9087716bdb989baeb2301a988cb43f1dbdb311
unknown <franze@Lenovo-PC.ruv.is>
2016-02-22 10:27:20 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
@@ -1,22 +1,22 @@
 <?php
-//$image_url = '';
+$image_url = '';
 $image_url2 = '';
 
 //Til að fá atom id
-//$atom_id = $content['field_custom_banner_image']['#items'][0]['sid'];
+$atom_id = $content['field_custom_banner_image']['#items'][0]['sid'];
 $atom_id2 = $content['field_background_repeat']['#items'][0]['sid'];
 
 //til að ná í atom
-//$atom = scald_atom_load($atom_id);
+$atom = scald_atom_load($atom_id);
 $atom2 = scald_atom_load($atom_id2);
 
 if(!empty($atom)) {
         //Til að ná í myndaurlið í réttum stíl
-	//$image_url = image_style_url('hero_image_subpage', $atom->file_source);
+	$image_url = image_style_url('hero_image_subpage', $atom->file_source);
 }
 if(!empty($atom2)) {
         //Til að ná í myndaurlið í réttum stíl
-	$image_url2 = image_style_url('hero_image_subpage', $atom->file_source);
+	$image_url2 = image_style_url('hero_image_subpage', $atom2->file_source);
 }
 
 ?>
