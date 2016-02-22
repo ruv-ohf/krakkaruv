b1dff710a9ebb5cb716ba953de8d571a7313cad3
unknown <franze@Lenovo-PC.ruv.is>
2016-02-22 14:01:05 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--banner_item.tpl.php
@@ -63,14 +63,14 @@ if(!empty($atom2)) {
 <?php if ($hlekkur): ?>
 	<a href="<?php print render($hlekkur); ?>">
 		<div class="paragraphs-items-wrapper-inner">
-			<?php print render($content['field_breakpoint']); ?>
 			<?php print render($content['field_absolute_positioned_elemen']); ?>
+			<?php print render($content['field_breakpoint']); ?>
 		</div>
 	</a>
 <?php else: ?>
 	<div class="paragraphs-items-wrapper-inner">
-		<?php print render($content['field_breakpoint']); ?>
 		<?php print render($content['field_absolute_positioned_elemen']); ?>
+		<?php print render($content['field_breakpoint']); ?>
 	</div>
 <?php endif; ?>
 
