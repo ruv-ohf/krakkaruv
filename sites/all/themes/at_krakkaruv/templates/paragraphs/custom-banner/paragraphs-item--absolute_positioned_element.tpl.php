fc2e5c330391a31cda6f8fcbc73c14b502feaba8
unknown <franze@Lenovo-PC.ruv.is>
2016-02-23 12:49:39 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--absolute_positioned_element.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--absolute_positioned_element.tpl.php
@@ -58,7 +58,7 @@ if(!empty($atom)) {
 
 	<div class="absolute-image-<?php print $atom_id; ?>">
 		<style>
-			@media only screen and (max-width: " . render($breakpoint_max); ?>px){
+			@media only screen <?php print "and (max-width: " . render($breakpoint_max); ?>px){
 				.absolute-image-<?php print $atom_id; ?> {
 					background: url(<?php print $image_url; ?>) no-repeat center center;
 					width: <?php print($atom->scald_thumbnail['und'][0]['metadata']['width']); ?>px;
