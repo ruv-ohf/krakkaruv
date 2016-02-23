64d93a8547fedb2b47ee8c3f65dfe31de726eff2
unknown <franze@Lenovo-PC.ruv.is>
2016-02-23 12:26:46 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--absolute_positioned_element.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--absolute_positioned_element.tpl.php
@@ -22,7 +22,7 @@ if(!empty($atom)) {
 
 ?>
 
-<?php if (($breakpoint)&&($breakpoint_max)): ?>
+<?php if (($breakpoint)||($breakpoint_max)): ?>
 
 	<div class="absolute-image-<?php print $atom_id; ?>">
 		<style>
