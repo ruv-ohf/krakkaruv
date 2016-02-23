64986a16610e8a9aa31ad50e46db6b850676ad49
unknown <franze@Lenovo-PC.ruv.is>
2016-02-23 08:17:56 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-items--field_absolute_positioned_elemen.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-items--field_absolute_positioned_elemen.tpl.php
@@ -1,28 +1,3 @@
-<?php hide($content['field_custom_banner_image']); ?>
-
-<?php
-
-$image_url 	= '';
-
-//Til að fá atom id
-$atom_id 	= $content['field_custom_banner_image']['#items'][0]['sid'];
-
-//til að ná í atom
-$atom 		= scald_atom_load($atom_id);
-
-if(!empty($atom)) {
-        //Til að ná í myndaurlið í réttum stíl
-	$image_url = image_style_url('hero_image_subpage', $atom->file_source);
-}
-
-?>
-
+<div class="franz">
 <?php print $content; ?>
-
-<style>
-	.absolute-image {
-		background: url(<?php print $image_url; ?>) no-repeat center center;
-		width: 	<?php print($atom->scald_thumbnail['und'][0]['metadata']['width']); ?>px;
-		height: <?php print($atom->scald_thumbnail['und'][0]['metadata']['height']); ?>px;
-	}
-</style>
+</div>
