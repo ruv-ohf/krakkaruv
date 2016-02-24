c12f24ef276e03d2651b7bda4859c5617b357ee6
unknown <franze@Lenovo-PC.ruv.is>
2016-02-24 11:52:50 +0000

    working on custom banner

--- sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--absolute_positioned_element.tpl.php
+++ sites/all/themes/at_krakkaruv/templates/paragraphs/custom-banner/paragraphs-item--absolute_positioned_element.tpl.php
@@ -14,7 +14,7 @@
 $image_url 	= '';
 
 //Til að fá atom id
-$test = $content['field_custom_banner_image']->['#object'];
+$test = $content['field_custom_banner_image'];
 $atom_id 	= $content['field_custom_banner_image']['#items'][0]['sid'];
 
 //til að ná í atom
