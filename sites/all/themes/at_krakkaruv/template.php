b012baa620e0e0023f2704d1e7924d163b4ca0b5
unknown <franze@Lenovo-PC.ruv.is>
2016-05-18 11:15:48 +0000

    styling forsetakosningar 2016

--- sites/all/themes/at_krakkaruv/template.php
+++ sites/all/themes/at_krakkaruv/template.php
@@ -99,3 +99,32 @@ function at_krakkaruv_process_page(&$variables) {
         drupal_add_js(drupal_get_path('theme', 'at_krakkaruv') . '/scripts/remove_cookie.js');
     }
 }
+
+function at_krakkaruv_preprocess_html(&$variables) {
+  if(arg(0)=='node' && is_numeric(arg(1))) {
+    $node = node_load(arg(1));  
+    $results = _MYTHEME_taxonomy_node_get_terms($node); 
+    if(is_array($results)) {
+      foreach ($results as $item) { 
+        $variables['classes_array'][] = "taxonomy-".strtolower(drupal_clean_css_identifier($item->name));
+      }
+    }
+  }
+}
+ 
+function at_krakkaruv_taxonomy_node_get_terms($node, $key = 'tid') {
+  static $terms;
+  if (!isset($terms[$node->vid][$key])) {
+    $query = db_select('taxonomy_index', 'r');
+    $t_alias = $query->join('taxonomy_term_data', 't', 'r.tid = t.tid');
+    $v_alias = $query->join('taxonomy_vocabulary', 'v', 't.vid = v.vid');
+    $query->fields( $t_alias );
+    $query->condition("r.nid", $node->nid);
+    $result = $query->execute();
+    $terms[$node->vid][$key] = array();
+    foreach ($result as $term) {
+      $terms[$node->vid][$key][$term->$key] = $term;
+    }
+  }
+  return $terms[$node->vid][$key];
+}
