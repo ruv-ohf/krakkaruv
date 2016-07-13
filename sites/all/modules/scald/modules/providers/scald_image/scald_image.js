01b7d021e31a798e286daa5d7d35fe7a87dd6095
Hilmar The Drupal Viking <drupalviking@gmail.com>
2016-07-13 12:38:23 +0000

    Scald update

--- sites/all/modules/scald/modules/providers/scald_image/scald_image.js
+++ sites/all/modules/scald/modules/providers/scald_image/scald_image.js
@@ -6,7 +6,7 @@
           infoTab.add({
             id: 'txtLink',
             type: 'text',
-            label: 'Link',
+            label: Drupal.t('Link'),
             // "Link" edits the 'link' property in the options JSON string.
             setup: function (widget) {
               var options = JSON.parse(widget.data.options);
@@ -23,6 +23,33 @@
               }
               else {
                 delete options.link;
+              }
+              widget.setData('options', JSON.stringify(options));
+            }
+          });
+        });
+        Drupal.dndck4.addOption('cmbLinkTarget', 'image', 'atom', 'scald_image', function (infoTab, dialogDefinition) {
+          infoTab.add({
+            id: 'cmbLinkTarget',
+            type: 'select',
+            label: Drupal.t('Link Target'),
+            items: [[Drupal.t('None'), '_self'], [Drupal.t('Blank'), '_blank'], [Drupal.t('Parent'), '_parent']],
+            // "Link Target" edits the 'linkTarget' property in the options JSON string.
+            setup: function (widget) {
+              var options = JSON.parse(widget.data.options);
+              if (options.linkTarget) {
+                this.setValue(options.linkTarget);
+              }
+            },
+            commit: function (widget) {
+              // Copy the current options into a new object,
+              var options = JSON.parse(widget.data.options);
+              var value = this.getValue();
+              if (value != '') {
+                options.linkTarget = value;
+              }
+              else {
+                delete options.linkTarget;
               }
               widget.setData('options', JSON.stringify(options));
             }
