80da0e1f30547a846a9a6a61ebfbbba21e0ffa50
Franz Eiriksson <franz@kosmosogkaos.is>
2016-10-11 23:49:36 +0000

    added html.tpl file for drupal

---
+++ sites/all/themes/kodinn/templates/html.tpl.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?>"<?php print $attributes; ?>>
+<div id="skip-link">
+   <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+</div>
+<?php print $page_top; ?>
+<?php print $page; ?>
+<?php print $page_bottom; ?>
+</body>
+</html>
