e31fc0e7ea82411fb2cd84231ef70c9cc0190085
Franz Eiriksson <franz@kosmosogkaos.is>
2016-10-15 13:58:00 +0000

    added js for menu

--- sites/all/themes/kodinn/scripts/scripts.js
+++ sites/all/themes/kodinn/scripts/scripts.js
@@ -8,6 +8,47 @@
             $('.main-menu .content').toggleClass('active')
         });
 
+        /* -------------------------------------------
+ 			Main Header hide and reveal on scroll
+ 		------------------------------------------- */
+
+		var didScroll;
+    	var lastScrollTop = 0;
+    	var delta = 5;
+    	var navbarHeight = $('.main-menu').outerHeight();
+
+    	$(window).scroll(function(event){
+        	didScroll = true;
+    	});
+
+    	setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    	}, 250);
+
+    	function hasScrolled() {
+        	var st = $(this).scrollTop();
+        
+        	if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        	if (st > lastScrollTop && st > navbarHeight){
+            	$('.main-menu').removeClass('main-menu-down').addClass('main-menu-up');
+        	} 
+
+        	else {
+            	if(st + $(window).height() < $(document).height()) {
+                	$('.main-menu').removeClass('main-menu-up').addClass('main-menu-down');
+            	}
+        	}
+
+        	lastScrollTop = st;
+    	}
+
+
+
 
         }
     };
